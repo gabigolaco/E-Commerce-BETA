6a63f30190ed4326aba8e241e5745caf9b16196e
Gabi <63184250+GaabsPlaays@users.noreply.github.com>
2024-10-25 15:57:46 -0300

    again

--- script.js
+++ script.js
@@ -12,18 +12,18 @@ window.addEventListener('load', loadAnimation)
     function loadAnimation() {
     let burgerOne = document.getElementById('burgerOne')   
     let position = 50
-    burgerOne.style.display = 'flex'
     burgerOne.style.transition = '1s'
     
-
+    
     burgerOne.style.transform = `translateX(${position}px)`;
-
+    
     let anim = setInterval(() => {
-        if (position <= -700) {
-            clearInterval(anim)
-        } else {
-            position -= 30
-            burgerOne.style.transform = `translateX(${position}px)`;
-        }
-    }, 1)
+            burgerOne.style.display = 'flex'
+            if (position <= -700) {
+                clearInterval(anim)
+            } else {
+                position -= 30
+                burgerOne.style.transform = `translateX(${position}px)`;
+            }
+    }, 10)
     }
