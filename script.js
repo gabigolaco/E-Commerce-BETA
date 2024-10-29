b38cf6e21feb28cc09ef95eda4c5e20c167b8c38
Gabi <63184250+GaabsPlaays@users.noreply.github.com>
2024-10-29 15:18:33 -0300

    fixed bugs

--- script.js
+++ script.js
@@ -5,25 +5,84 @@ let prevButton = document.getElementById('prevButton')
 
 orderButton.addEventListener('click', () => {})
 readMoreButton.addEventListener('click', () => {})
+nextButton.addEventListener('click', nextImg)
 prevButton.addEventListener('click', () => {})
 
 window.addEventListener('load', loadAnimation)
 
-    function loadAnimation() {
-    let burgerOne = document.getElementById('burgerOne')   
-    let position = 50
+let burgerOne = document.getElementById('burgerOne')   
+let burgerTwo = document.getElementById('burgerTwo')   
+let burgerThree = document.getElementById('burgerThree')   
+
+function loadAnimation() {
+    nextButton.disabled = true
+    prevButton.disabled = true
+
+    setTimeout (() => {
+        nextButton.disabled = false
+        prevButton.disabled = false
+    }, 2000)
+
+    let position = 1200
     burgerOne.style.transition = '1s'
     
     
-    burgerOne.style.transform = `translateX(${position}px)`;
+    burgerOne.style.left = position + 'px'
     
     let anim = setInterval(() => {
             burgerOne.style.display = 'flex'
-            if (position <= -700) {
+            if (position <= 600) {
                 clearInterval(anim)
             } else {
                 position -= 30
-                burgerOne.style.transform = `translateX(${position}px)`;
+                burgerOne.style.left = position + 'px'
+                console.log(position)
             }
     }, 10)
     }
+
+function nextImg() {
+    nextButton.disabled = true
+    
+    setTimeout(() => {
+        nextButton.disabled = false // COOLDOWN
+    }, 2000);
+
+    let position = 600
+    burgerOne.style.transition = '2s'
+    if (burgerOne.offsetLeft === position) {
+        let anim = setInterval(() => {
+            if (position >= 1400) {
+                clearInterval(anim)
+                animationTwo(position)
+            } else {
+                position += 200
+                burgerOne.style.left = position + 'px'
+                console.log(position)
+                setTimeout(() => {
+                    burgerOne.style.display = 'none'
+                }, 1500);
+
+
+            }
+    }, 10)
+    }
+}
+
+function animationTwo(posicao) {
+    burgerTwo.style.display = 'flex'
+
+    setTimeout(() => {
+        let anim = setInterval(() => {
+            burgerTwo.style.transition = '2s'
+            if (posicao <= 600) {
+                clearInterval(anim)
+            } else {
+                posicao -= 15
+                burgerTwo.style.left = posicao + 'px'
+            }
+        }, 10)
+    }, 1500);
+   
+
+}
